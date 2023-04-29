5f930d391d7110262c402efb855e60446ccffef8
Crydo <132129299+Crydo@users.noreply.github.com>
2023-04-29 13:52:13 +0500

    Update pin-pong.py
    Добавление шрифта

--- pin-pong.py
+++ pin-pong.py
@@ -33,4 +33,18 @@ black = (200, 255, 255)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_height, win_width))
-window.fill(back)
+window.fill(black)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('rocket.png', 30, 200, 4, 50, 150)
+racket2 = Player('rocket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
